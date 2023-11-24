7474cb78237a66cb4ab0524dd21c6781d78ff296
tr1sm0s1n <mobinm.mohanan@gmail.com>
2023-11-24 20:22:31 +0530

    💡 | uncommented sepolia config

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -8,10 +8,10 @@ module.exports = {
     local: {
       url: "http://127.0.0.1:8545",
     },
-    // sepolia: {
-    //   url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.API_KEY}`,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   paths: {
     artifacts: "../api/src/artifacts",
